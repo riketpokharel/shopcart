f44c5dee20dab18da175b0c257b5efb77caf269a
Riket <pokharelriket777@gmail.com>
2026-01-27 23:47:46 +0545

    feat: fixed sanity issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,13 @@
-import type { Metadata } from "next";
-
-import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
-
-export const metadata: Metadata = {
-  title: {
-    template: "%s - Shopcart online store",
-    default: "Shopcart online store",
-  },
-  description: "Shopcart online store, Your one stop shop for all your needs",
-};
-
-export default function RootLayout({
+const RootLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) => {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className="font-poppins antialiased">
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">{children}</main>
-            <Footer />
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className="font-poppins antialiased">{children}</body>
+    </html>
   );
-}
+};
+
+export default RootLayout;
